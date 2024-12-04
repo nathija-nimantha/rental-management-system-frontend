7d01f3df000596620a0bda70394294f9a5c22bc1
nathija-nimantha <nathija.max99@gmail.com>
2024-12-04 15:37:19 +0530

    Implemented Home-[age and add routerlink to nav-bar

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {
-
+export class HomePageComponent implements OnInit {
+  ngOnInit(): void {
+    AOS.init();
+  }
 }
