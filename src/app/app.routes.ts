8a8efe1b72c15e447c854aabb3e9e223f3e436a1
nathija-nimantha <nathija.max99@gmail.com>
2024-12-04 15:18:04 +0530

    Updated app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,23 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ManageCustomersComponent } from './pages/manage-customers/manage-customers.component';
 import { ManageItemsComponent } from './pages/manage-items/manage-items.component';
+import { ManageRentalsComponent } from './pages/manage-rentals/manage-rentals.component';
 
 export const routes: Routes = [
   {
     path: "",
     component: HomePageComponent
-},
-{
-  path: "/customers",
-  component: ManageCustomersComponent
-},
-{
-  path: "/items",
-  component: ManageItemsComponent
-}
+  },
+  {
+    path: "customers",
+    component: ManageCustomersComponent
+  },
+  {
+    path: "items",
+    component: ManageItemsComponent
+  },
+  {
+    path: "rentals",
+    component: ManageRentalsComponent
+  }
 ];
